08666b881cdfeb29124d850b0ac9816d1cc69c06
Alex Toth <tothalex95@gmail.com>
2016-10-18 15:50:01 +0200

    tabacco category has been addded. The party shall begin.

--- registrar-model/src/main/java/hu/uni/miskolc/iit/swtest/registrar/model/Category.java
+++ registrar-model/src/main/java/hu/uni/miskolc/iit/swtest/registrar/model/Category.java
@@ -2,7 +2,7 @@ package hu.uni.miskolc.iit.swtest.registrar.model;
 
 public enum Category {
 
-	FOOD, CLOTHING, ALCOHOL
+	FOOD, CLOTHING, ALCOHOL, TABACCO
 	
 	/*
 	 * S ingle Responsibility Principle - 1 osztály egy feladat/felelősség
