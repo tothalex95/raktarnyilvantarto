aba55783b309adc08836b0092c2adef8b6e6aae7
Sátán Ádám <Stn dm@DESKTOP-N6GF4GS>
2016-10-22 17:47:31 +0200

    minor fix

--- registrar-model/src/main/java/hu/uni/miskolc/iit/swtest/registrar/model/IndustrialEquipment.java
+++ registrar-model/src/main/java/hu/uni/miskolc/iit/swtest/registrar/model/IndustrialEquipment.java
@@ -89,26 +89,4 @@ public class IndustrialEquipment extends Product {
 	{
 		return dangerLevel;
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + productNumber;
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		IndustrialEquipment other = (IndustrialEquipment) obj;
-		if (productNumber != other.productNumber)
-			return false;
-		return true;
-	}
 }
