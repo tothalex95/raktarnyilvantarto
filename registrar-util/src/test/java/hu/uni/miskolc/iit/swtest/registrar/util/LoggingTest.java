b21c187db0ecbc21ad8f295214d59a3c24e44eb5
Alex Martossy <alexwankenobi69@gmail.com>
2016-10-22 10:58:33 +0200

    - log4j2 has been configured and submitted for review

--- registrar-util/src/test/java/hu/uni/miskolc/iit/swtest/registrar/util/LoggingTest.java
+++ registrar-util/src/test/java/hu/uni/miskolc/iit/swtest/registrar/util/LoggingTest.java
@@ -1,7 +1,17 @@
 package hu.uni.miskolc.iit.swtest.registrar.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.*;
+
 public class LoggingTest {
 
+	private static Logger LOG = LogManager.getFormatterLogger(LoggingTest.class);
 	
-	
+	@Test
+	public void test() {
+		LOG.info("info test");
+		LOG.warn("warn test");
+		LOG.error("error test");
+	}
 }
