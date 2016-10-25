81099a270a53713b70d5f66379b2d23099d9ee46
x <x@x-VirtualBox>
2016-10-25 15:04:46 +0200

    - log4j2 finalized and tested for all individual loggers, with seemingly proper outputs

--- registrar-util/src/test/java/hu/uni/miskolc/iit/swtest/registrar/util/LoggingTest.java
+++ registrar-util/src/test/java/hu/uni/miskolc/iit/swtest/registrar/util/LoggingTest.java
@@ -6,13 +6,50 @@ import org.junit.*;
 
 public class LoggingTest {
 
-	private static Logger LOG = LogManager.getFormatterLogger(LoggingTest.class);
-	
+	private static Logger rootLOG = LogManager.getFormatterLogger(LoggingTest.class);
+	private static Logger contrLOG = LogManager.getLogger("hu.uni.miskolc.iit.swtest.registrar.controller");
+	private static Logger modLOG = LogManager.getLogger("hu.uni.miskolc.iit.swtest.registrar.model");
+	private static Logger persLOG = LogManager.getLogger("hu.uni.miskolc.iit.swtest.registrar.persist");
+	private static Logger servLOG = LogManager.getLogger("hu.uni.miskolc.iit.swtest.registrar.service");
+
 	@Test
-	public void test() {
-		LOG.info("info test");
-		LOG.warn("warn test");
-		LOG.error("error test");
-		LOG.fatal("Tündi");
+	public void testRoot() {
+		rootLOG.info("root info test");
+		rootLOG.warn("root warn test");
+		rootLOG.error("root error test");
+		rootLOG.fatal("root fatal test");
+	}
+
+	@Test
+	public void testController() {
+		contrLOG.info("controller info test");
+		contrLOG.warn("controller warn test");
+		contrLOG.error("controller error test");
+		contrLOG.fatal("controller fatal test");
+
+	}
+
+	@Test
+	public void testModel() {
+		modLOG.info("model info test");
+		modLOG.warn("model warn test");
+		modLOG.error("model error test");
+		modLOG.fatal("model fatal test");
+	}
+
+	@Test
+	public void testPersist() {
+		persLOG.info("persist info test");
+		persLOG.warn("persist warn test");
+		persLOG.error("persist error test");
+		persLOG.fatal("persist fatal test");
+	}
+
+	@Test
+	public void testService() {
+		servLOG.info("service info test");
+		servLOG.warn("service warn test");
+		servLOG.error("service error test");
+		servLOG.fatal("service fatal test");
 	}
 }
