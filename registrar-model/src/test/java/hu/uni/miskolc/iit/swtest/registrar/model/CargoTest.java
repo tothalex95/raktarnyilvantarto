86f71b89207fdc5cd30ae96ea2d82e3f6faa826a
Alex Toth <tothalex95@gmail.com>
2016-11-08 15:29:02 +0100

    box and cargo got an id member

--- registrar-model/src/test/java/hu/uni/miskolc/iit/swtest/registrar/model/CargoTest.java
+++ registrar-model/src/test/java/hu/uni/miskolc/iit/swtest/registrar/model/CargoTest.java
@@ -14,45 +14,45 @@ public class CargoTest {
 	//public Cargo(Collection<Box> boxes, Address source, Address destination)
 	@Test(expected = NullPointerException.class)
 	public void testCargoConstructorWithNullBoxes(){
-		new Cargo(null, new Address("A", "B", "C", 3),  new Address("A", "B", "C", 3));
+		new Cargo(1, null, new Address("A", "B", "C", 3),  new Address("A", "B", "C", 3));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testCargoConstructorWithEmptyBoxes(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		new Cargo(boxes,  new Address("A", "B", "C", 3),  new Address("A", "B", "C", 3));
+		new Cargo(1, boxes,  new Address("A", "B", "C", 3),  new Address("A", "B", "C", 3));
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void testCargoConstructorWithNullSource(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
-		new Cargo(boxes,null, new Address("A", "B", "C", 3));
+		boxes.add(new Box(1, new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
+		new Cargo(1, boxes,null, new Address("A", "B", "C", 3));
 	}
 	@Test(expected = IllegalArgumentException.class)
 	public void testCargoConstructorWithNullDestination(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
-		new Cargo(boxes, new Address("A", "B", "C", 3), null);
+		boxes.add(new Box(1, new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
+		new Cargo(1, boxes, new Address("A", "B", "C", 3), null);
 	}
 	@Test
 	public void testGetBoxes(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
-		Cargo a = new Cargo(boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
+		boxes.add(new Box(1, new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
+		Cargo a = new Cargo(1, boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
 		assertEquals(boxes, a.getBoxes());
 	}
 	@Test
 	public void testGetSource(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
-		Cargo a = new Cargo(boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
+		boxes.add(new Box(1, new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
+		Cargo a = new Cargo(1, boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
 		assertEquals(new Address("A", "B", "C", 3), a.getSource());
 	}
 	@Test
 	public void testGetDestination(){
 		Collection<Box> boxes = new ArrayList<Box>();
-		boxes.add(new Box(new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
-		Cargo a = new Cargo(boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
+		boxes.add(new Box(1, new Product(1, Category.FOOD, "vaj", Manufacturer.APPLE, new Size(3), 25, Restriction.getFlagFromRestrictions(EnumSet.noneOf(Restriction.class))),1,BoxSize.BIG));
+		Cargo a = new Cargo(1, boxes,new Address("A", "B", "C", 3), new Address("A", "B", "C", 3));
 		assertEquals(new Address("A", "B", "C", 3), a.getDestination());
 	}
 
