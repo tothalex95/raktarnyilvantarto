64c6e566e2710f5901cc030c3aa6c3948544316c
Alex Toth <tothalex95@gmail.com>
2016-11-15 15:32:05 +0100

    create table sql script

---
+++ registrar-persist-mysql/src/main/resources/createTables.sql
@@ -0,0 +1,58 @@
+create table productTable (
+	id integer primary key,
+	category enum('FOOD','FURNITURE','NARCOTICS','INDUSTRIAL_EQUIPMENT'),
+	name varchar(50),
+	manufacturer enum('APPLE','BRAZZERS','UMBRELLACORP'),
+	sizex integer,
+	sizey integer,
+	sizez integer,
+	weight float,
+	restriction integer
+);
+
+create table customerTable (
+	id integer primary key,
+	name varchar(50),
+	address varchar(100)
+);
+
+create table positionsTable (
+	id integer primary key,
+	productId integer,
+	place varchar(10),
+	foreign key (productId) references productTable(id) on delete cascade
+);
+
+create table boxTable (
+	id integer primary key,
+	product integer,
+	count integer,
+	boxSize enum('SMALL','MEDIUM','LARGE'),
+	foreign key (product) references productTable(id) on delete cascade
+);
+
+create table orderTable (
+	id integer primary key,
+	orderId integer,
+	product integer,
+	customer integer,
+	count integer,
+	foreign key (product) references productTable(id) on delete cascade,
+	foreign key (customer) references customerTable(id) on delete cascade
+);
+
+create table cargoTable (
+	id integer primary key,
+	source integer,
+	destination integer, 
+	foreign key (source) references customerTable(id) on delete cascade,
+	foreign key (destination) references customerTable(id) on delete cascade
+);
+
+create table cargo_boxTable (
+	id integer primary key,
+	cargoId integer,
+	boxId integer,
+	foreign key (cargoId) references cargoTable(id) on delete cascade,
+	foreign key (boxId) references boxTable(id) on delete cascade
+);
