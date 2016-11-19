9e19c7124a88c0dad949890e8c39c87779484bb6
iasatan <x@x-VirtualBox>
2016-11-19 16:44:29 +0100

    OrderMapper aplha verion

--- registrar-persist-mysql/src/main/java/hu/uni/miskolc/iit/swtest/registrar/persist/mysql/mapper/OrderMapper.java
+++ registrar-persist-mysql/src/main/java/hu/uni/miskolc/iit/swtest/registrar/persist/mysql/mapper/OrderMapper.java
@@ -1,5 +1,27 @@
 package hu.uni.miskolc.iit.swtest.registrar.persist.mysql.mapper;
 
+import java.util.Collection;
+
+import org.apache.ibatis.annotations.Param;
+
+import hu.uni.miskolc.iit.swtest.registrar.model.Order;
+
+
 public interface OrderMapper {
 
+	public Collection<Order> selectOrders();
+	
+	public Collection<Order> selectOrdersByOrderId(int orderId);
+	
+	public Collection<Order> selectOrdersByProductId(int product);
+	
+	public Collection<Order> selectOrdersByCustomerID(int customer);
+	
+	public Collection<Order> selectOredersByCount(int count);
+	
+	//public Order selectOrdersById(int id);
+	
+	//public void updateOrder(@Param("order") Order order);
+	
+	public void deleteOrder(@Param("order") Order order);	
 }
